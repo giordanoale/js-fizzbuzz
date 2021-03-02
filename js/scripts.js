f962abb46df5749fc0d691638e96d905c9019607
giordanoale <a.giordano89@gmail.com>
2021-03-02 17:18:07 +0100

    Gli elementi compaiono sia sulla homepage che sul console log

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,30 @@
 // Ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
+var ul = document.getElementById("risultato");
+
 for (var i = 1; i <= 100; i++) {
 
   if (i % 3 == 0 && i % 5 == 0) {
     console.log("FizzBuzz");
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode("FizzBuzz"));
+    ul.appendChild(li);
   } else if (i % 3 == 0) {
     console.log("Fizz");
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode("Fizz"));
+    ul.appendChild(li);
   } else if (i % 5 == 0) {
     console.log("Buzz");
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode("Buzz"));
+    ul.appendChild(li);
   } else {
     console.log(i);
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(i));
+    ul.appendChild(li);
   }
 
 }
